a4b678ec4407cee8d67f3dd85f64b3d9d642e4a3
Oleksii Shcherbak <qksardas001@mail.com>
2026-03-07 15:36:25 +0200

    feat: add play, genre and actor models

--- plays/admin.py
+++ plays/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from plays.models import Actor, Genre, Play
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    search_fields = ("first_name", "last_name")
+
+
+@admin.register(Play)
+class PlayAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    search_fields = ("title",)
+    filter_horizontal = ("genres", "actors")
--- plays/models.py
+++ plays/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Actor(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ("first_name", "last_name")
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Play(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    genres = models.ManyToManyField(Genre, related_name="plays")
+    actors = models.ManyToManyField(Actor, related_name="plays")
+
+    def __str__(self) -> str:
+        return self.title
