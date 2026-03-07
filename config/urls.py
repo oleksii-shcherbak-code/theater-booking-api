07de33e9974a623f0115646c114c420dcd06eb90
Oleksii Shcherbak <qksardas001@mail.com>
2026-03-07 17:51:14 +0200

    fix: working API routing and project setup

--- config/urls.py
+++ config/urls.py
@@ -4,8 +4,12 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
     path("api/users/", include("users.urls")),
     path("api/auth/", include("users.auth_urls")),
+
+    path("api/", include("booking.urls")),
+
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 ]
